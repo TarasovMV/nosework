918a1e1fed40f5f4711c0daefe8fc91f71079d20
Maxim Tarasov <31613333+TarasovMV@users.noreply.github.com>
2023-09-21 00:59:28 +0300

    feat: training form

--- apps/app/src/app/pages/training/training.page.ts
+++ apps/app/src/app/pages/training/training.page.ts
@@ -2,18 +2,73 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterLink} from '@angular/router';
 import {TuiLetModule} from '@taiga-ui/cdk';
-import {FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {
+    TuiDataListWrapperModule,
+    TuiInputDateModule,
+    TuiInputModule,
+    TuiInputNumberModule,
+    TuiInputSliderModule,
+    TuiInputTimeModule,
+    TuiRadioBlockModule,
+    TuiSelectModule,
+    TuiTextareaModule,
+} from '@taiga-ui/kit';
+import {
+    TuiButtonModule,
+    TuiGroupModule,
+    TuiTextfieldControllerModule,
+} from '@taiga-ui/core';
+import {BehaviorSubject} from 'rxjs';
+import {CATEGORIES} from '@nw-app/constants';
 
 @Component({
     selector: 'nw-training',
     standalone: true,
-    imports: [CommonModule, RouterLink, TuiLetModule, ReactiveFormsModule],
+    imports: [
+        CommonModule,
+        RouterLink,
+        TuiLetModule,
+        ReactiveFormsModule,
+        TuiInputDateModule,
+        TuiInputTimeModule,
+        TuiInputModule,
+        TuiInputSliderModule,
+        TuiTextfieldControllerModule,
+        TuiInputNumberModule,
+        TuiTextareaModule,
+        TuiButtonModule,
+        TuiSelectModule,
+        TuiDataListWrapperModule,
+        TuiGroupModule,
+        TuiRadioBlockModule,
+    ],
     templateUrl: './training.page.html',
     styleUrls: ['./training.page.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TrainingPage {
-    protected readonly form = new FormGroup({});
+    protected readonly categories = CATEGORIES;
+
+    protected readonly submitLoader$ = new BehaviorSubject<boolean>(false);
+
+    protected readonly form = new FormGroup({
+        date: new FormControl(),
+        time: new FormControl(),
+        place: new FormControl(),
+        target: new FormControl(),
+        category: new FormControl(),
+        searchType: new FormControl(),
+        source: new FormControl(),
+        count: new FormControl(1),
+        sourcePlace: new FormControl(),
+        waitTime: new FormControl(),
+        distractions: new FormControl(),
+        temperature: new FormControl(),
+        humidity: new FormControl(),
+        wind: new FormControl(),
+        conclusion: new FormControl(),
+    });
 
     save(e: Event) {
         e.stopPropagation();
--- apps/app/src/app/pages/trainings/trainings.page.ts
+++ apps/app/src/app/pages/trainings/trainings.page.ts
@@ -1,14 +1,21 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 import {TuiLetModule} from '@taiga-ui/cdk';
+import {TuiButtonModule} from '@taiga-ui/core';
 
 @Component({
     selector: 'nw-trainings',
     standalone: true,
-    imports: [CommonModule, RouterLink, TuiLetModule],
+    imports: [CommonModule, RouterLink, TuiLetModule, TuiButtonModule],
     templateUrl: './trainings.page.html',
     styleUrls: ['./trainings.page.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TrainingsPage {}
+export class TrainingsPage {
+    constructor(private readonly router: Router) {}
+
+    create() {
+        this.router.navigate(['training']);
+    }
+}
